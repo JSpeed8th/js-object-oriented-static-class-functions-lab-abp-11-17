3e5ce1f2319c95245108791f3aa6d5e3a725c75b
JSpeed8th <jspeed8th@gmail.com>
2018-01-20 13:51:23 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,26 @@
 'use strict';
+
+class Question {
+
+  constructor (content){
+
+    this.content = content
+    this.save()
+  }
+
+  static All(){
+
+  return this._All;
+  }
+
+  static Find (id) {
+
+    return this.All()[id - 1]
+  }
+
+  save(){
+    this.constructor._All.push(this)
+  }
+}
+
+Question._All = [];
